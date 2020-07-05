583ed987330f55032dc8de5681388dba5d821225
a26232933 <a094868@gmail.com>
2020-07-05 10:49:01 +0800

    抓取參數

--- counter.js
+++ counter.js
@@ -13,17 +13,26 @@ $(document).ready(function () {
 
     console.log("視窗的上方:" + windowTop);
 
+
+    var number=$("#test").attr("data-counter-number");
+    var time=$("#test").attr("data-counter-time");
+    var offset=$("#test").attr("data-counter-offset");
+
+    // console.log("數字"+number);
+    // console.log("時間"+time);
+    // console.log("位移"+offset);
+
     // 判斷式
     // 如果 發生什麼 就....
     // if (條件) {程式}
-    if (windowTop > top) {
+    if (windowTop > top - offset) {
       // console.log("開始執行動畫...");
       $("#test").animate({
-        num: 999                           /* 數字 */
+        num: number                           /* 數字 */
       }, {
-        duration: 2000,                     /* 時間 */
+        duration: parseInt(time),             /* 時間  duration 需要數字 parseInt() 將字串變為整數*/  
         step: (now) => {                       /* 步驟 */
-          $("#test").text(Math.floor(now));             /* 文字(步驟)去小數點 Math.floor() */
+          $("#test").text(Math.floor(now));    /* 文字(步驟)去小數點 Math.floor() */
         }
       })
     }
